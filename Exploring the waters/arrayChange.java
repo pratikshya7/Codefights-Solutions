fb7cdbac8ef2cf92d6d3a7c7e23c112410916dce
Pratikshya Timalsina <31149834+pratikshya7@users.noreply.github.com>
2019-03-19 08:05:41 -0500

    Create arrayChange.java

---
+++ Exploring the waters/arrayChange.java
@@ -0,0 +1,26 @@
+/*
+QUESTION:
+You are given an array of integers. On each move you are allowed to increase exactly one of its element by one. Find the minimal number of moves required to obtain a strictly increasing sequence from the input.
+
+Example
+
+For inputArray = [1, 1, 1], the output should be
+arrayChange(inputArray) = 3.
+
+SOLUTION:
+
+*/
+
+int arrayChange(int[] inputArray) {
+  int moves = 0;
+	int diff = 0;
+	for( int i = 1; i<inputArray.length;i++) {	
+		if((inputArray[i] <= inputArray[i-1])){	
+        diff = Math.abs(inputArray[i] -inputArray[i-1]);
+        inputArray[i] = inputArray[i-1] + 1;
+        moves++;
+        moves += diff;
+       }
+    }
+ return moves;
+}
